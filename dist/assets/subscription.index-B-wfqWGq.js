add710b04f6529bf4f892d26de51189bbac42bd9
nischalbhandari12 <nischal.bhandari@mentorfriends.com>
2025-07-09 13:21:05 +0545

    version updatE

--- dist/assets/subscription.index-B-wfqWGq.js
+++
@@ -1,4 +0,0 @@
-import{y as o}from"./mftsccs-browser-DU86KKx4.js";import{m as s}from"./mainView.class-C4NLGCWW.js";import{e}from"./index-C4Kd4duQ.js";import{h as n}from"./modal.service-Ct6s_15E.js";import{N as a}from"./navbar.component-DWbtBhLa.js";import"./codemirror-B3vgQINq.js";import"./vendor-Cs_0E_Xj.js";import"./ckeditor-Cw9bcqxY.js";import"./eslint4b-BLWGvIOa.js";import"./prettier-ClL2wQep.js";import"./loginModal.service-CTV3TUA5.js";class x extends s{constructor(t){super(t),this.setTitle("Subscription")}async getHtml(){const t=await n(),r=await new a(t).getHtml();return setTimeout(async()=>{const i=document.getElementById("subscription-render");await o(e.subscriptionId,i)},0),`
-    ${r}
-        <div id="subscription-render"></div>
-        `}}export{x as default};
--- dist/assets/video-tutorial.index-D4jxceld.js
+++
@@ -1,6 +0,0 @@
-import{m as a}from"./mainView.class-C4NLGCWW.js";import{h as e}from"./modal.service-Ct6s_15E.js";import{N as r}from"./navbar.component-DWbtBhLa.js";import{e as m}from"./index-C4Kd4duQ.js";import{y as s}from"./mftsccs-browser-DU86KKx4.js";import"./codemirror-B3vgQINq.js";import"./vendor-Cs_0E_Xj.js";import"./ckeditor-Cw9bcqxY.js";import"./eslint4b-BLWGvIOa.js";import"./prettier-ClL2wQep.js";import"./loginModal.service-CTV3TUA5.js";class N extends a{constructor(t){super(t),this.setTitle("VideoTutorial")}async getHtml(){const t=await e(),i=await new r(t).getHtml();return setTimeout(async()=>{const o=document.getElementById("videoTutorial");o&&await s(m.video_tutorial_widget,o)},0),`
-           ${i}
-            <div class="video-tutorial" id="videoTutorial">
-              <!-- Video tutorial list -->
-            </div>
-        `}}export{N as default};
