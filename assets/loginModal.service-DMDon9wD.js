da92c8f1f0755cacfb231fc18715a1c18e943cce
Deepika-mentorfriends <deepikashrestha@mentorfriends.com>
2025-08-25 14:23:15 +0545

    update

--- assets/loginModal.service-DMDon9wD.js
+++ assets/loginModal.service-DMDon9wD.js
@@ -1,4 +1,4 @@
-import{A as I,$ as M}from"./mftsccs-browser-MShQrN6O.js";import{B as g,L as y,H as b,N as w,A as h}from"./index-Cql6_3k5.js";class S{constructor(){this.init()}init(){const s=this.getHtml();document.querySelector("body #app")?.insertAdjacentHTML("afterend",s)}getHtml(){return`
+import{A as I,$ as M}from"./mftsccs-browser-MShQrN6O.js";import{B as g,L as y,H as b,N as w,A as h}from"./index--BR_FAre.js";class S{constructor(){this.init()}init(){const s=this.getHtml();document.querySelector("body #app")?.insertAdjacentHTML("afterend",s)}getHtml(){return`
     <dialog id="login-dialog" class="login-modal">
       <div class="login-container">
         <button class="close-button" type="button">
