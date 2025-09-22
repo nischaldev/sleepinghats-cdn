22169f104c3b40a62fb6063504fcd800bbf90f3b
Deepika-mentorfriends <deepikashrestha@mentorfriends.com>
2025-09-22 09:40:03 +0545

    update

---
+++ assets/documentation.index-DsuXSkuz.js
@@ -0,0 +1,12 @@
+import{m}from"./mainView.class-C4NLGCWW.js";import{q as r}from"./index-T8V7Ehpu.js";import{o as e,q as c}from"./vendor-DEYWGvhS.js";import{H as n}from"./highlight-CFrhCzdo.js";import{N as l}from"./navbar.component-BZggnMlj.js";import"./mftsccs-browser-Cj5m6_BU.js";import"./codemirror-D1H9tReK.js";import"./eslint4b-BLWGvIOa.js";import"./prettier-ClL2wQep.js";import"./ckeditor-Cw9bcqxY.js";import"./loginModal.service-C5RYJL6E.js";class q extends m{constructor(a){super(a),this.setTitle("Documentation"),e.use(c({langPrefix:"hljs language-",highlight(i,t){const o=n.getLanguage(t)?t:"plaintext";return n.highlight(i,{language:o}).value}}))}async getHtml(){const a=await fetch("/images/doc/docs.md").then(s=>s.text()),i=e(a),t=await r();return`
+      ${await new l(t).getHtml()}
+      <div class="container pt-1">
+        <div class="row">
+          <div class="col-12">
+            <div class="documentation-content">
+              ${i}
+            </div>
+          </div>
+        </div>
+      </div>
+    `}}export{q as default};
---
+++ assets/video-tutorial.index-BdI5BELy.js
@@ -0,0 +1,6 @@
+import{m as a}from"./mainView.class-C4NLGCWW.js";import{q as e,H as r}from"./index-T8V7Ehpu.js";import{N as m}from"./navbar.component-BZggnMlj.js";import{m as s}from"./mftsccs-browser-Cj5m6_BU.js";import"./codemirror-D1H9tReK.js";import"./vendor-DEYWGvhS.js";import"./ckeditor-Cw9bcqxY.js";import"./eslint4b-BLWGvIOa.js";import"./prettier-ClL2wQep.js";import"./loginModal.service-C5RYJL6E.js";class H extends a{constructor(t){super(t),this.setTitle("VideoTutorial")}async getHtml(){const t=await e(),o=await new m(t).getHtml();return setTimeout(async()=>{const i=document.getElementById("videoTutorial");i&&await s(r.video_tutorial_widget,i)},0),`
+           ${o}
+            <div class="video-tutorial" id="videoTutorial">
+              <!-- Video tutorial list -->
+            </div>
+        `}}export{H as default};
