b21688914106f6e9066b2b034f464d577781498f
Deepika-mentorfriends <deepikashrestha@mentorfriends.com>
2025-09-22 15:57:20 +0545

    update

---
+++ assets/subscription.index-CXGEAULA.js
@@ -0,0 +1,4 @@
+import{m as s}from"./mftsccs-browser-Cj5m6_BU.js";import{m as n}from"./mainView.class-C4NLGCWW.js";import{q as o,H as e}from"./index-Bjcymr_s.js";import{N as a}from"./navbar.component-CzIoD5w9.js";import"./codemirror-D1H9tReK.js";import"./vendor-DEYWGvhS.js";import"./ckeditor-Cw9bcqxY.js";import"./eslint4b-BLWGvIOa.js";import"./prettier-ClL2wQep.js";import"./loginModal.service-DsfcjBw4.js";class N extends n{constructor(t){super(t),this.setTitle("Subscription")}async getHtml(){const t=await o(),i=await new a(t).getHtml();return setTimeout(async()=>{const r=document.getElementById("subscription-render");await s(e.subscriptionId,r)},0),`
+    ${i}
+        <div id="subscription-render"></div>
+        `}}export{N as default};
