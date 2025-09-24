639ddbe7a1571fefa94f3ba21a444277600a561c
Deepika-mentorfriends <deepikashrestha@mentorfriends.com>
2025-09-24 14:29:02 +0545

    update package

--- assets/subscription.index-BVuad7ap.js
+++
@@ -1,4 +0,0 @@
-import{m as s}from"./mftsccs-browser-Cj5m6_BU.js";import{m as n}from"./mainView.class-C4NLGCWW.js";import{q as o,H as e}from"./index-q0xBaVea.js";import{N as a}from"./navbar.component-WABa0P3W.js";import"./codemirror-D1H9tReK.js";import"./vendor-DEYWGvhS.js";import"./ckeditor-Cw9bcqxY.js";import"./eslint4b-BLWGvIOa.js";import"./prettier-ClL2wQep.js";import"./loginModal.service-WzLXQMUL.js";class N extends n{constructor(t){super(t),this.setTitle("Subscription")}async getHtml(){const t=await o(),i=await new a(t).getHtml();return setTimeout(async()=>{const r=document.getElementById("subscription-render");await s(e.subscriptionId,r)},0),`
-    ${i}
-        <div id="subscription-render"></div>
-        `}}export{N as default};
