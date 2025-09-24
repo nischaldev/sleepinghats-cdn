ffe59f92d97c885434529678713f0535e42a0792
Deepika-mentorfriends <deepikashrestha@mentorfriends.com>
2025-09-24 13:41:31 +0545

    update

--- assets/community.index-EOsdarfc.js
+++
@@ -1,6 +0,0 @@
-import{m as o}from"./mftsccs-browser-Cj5m6_BU.js";import{m as i}from"./mainView.class-C4NLGCWW.js";import{q as a,H as e}from"./index-Bjcymr_s.js";import{N as s}from"./navbar.component-CzIoD5w9.js";import"./codemirror-D1H9tReK.js";import"./vendor-DEYWGvhS.js";import"./ckeditor-Cw9bcqxY.js";import"./eslint4b-BLWGvIOa.js";import"./prettier-ClL2wQep.js";import"./loginModal.service-DsfcjBw4.js";class g extends i{constructor(t){super(t),this.setTitle("Community")}async getHtml(){const t=await a(),m=await new s(t).getHtml();return setTimeout(async()=>{const n=document.getElementById("communityContent");await o(e.discussion_community,n,"",!1)},0),`
-                    ${m}
-                    <div class="container py-5">
-                        <div id="communityContent"></div>
-                    </div>
-                `}}export{g as default};
