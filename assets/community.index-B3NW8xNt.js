4d160781622e4bdd13ca272eaaa89db023d3b618
Deepika-mentorfriends <deepikashrestha@mentorfriends.com>
2025-11-07 17:28:56 +0545

    update

--- assets/community.index-B3NW8xNt.js
+++
@@ -1,6 +0,0 @@
-import{j as o}from"./mftsccs-browser-kNLGvA95.js";import{m as i}from"./mainView.class-C4NLGCWW.js";import{q as a,H as e}from"./index-CggXStXB.js";import{N as s}from"./navbar.component-BrB-9qRs.js";import"./codemirror-D1H9tReK.js";import"./vendor-DEYWGvhS.js";import"./ckeditor-Cw9bcqxY.js";import"./eslint4b-BLWGvIOa.js";import"./prettier-ClL2wQep.js";import"./loginModal.service-G0xsO5qS.js";class g extends i{constructor(t){super(t),this.setTitle("Community")}async getHtml(){const t=await a(),m=await new s(t).getHtml();return setTimeout(async()=>{const n=document.getElementById("communityContent");await o(e.discussion_community,n,"",!1)},0),`
-                    ${m}
-                    <div class="container py-5">
-                        <div id="communityContent"></div>
-                    </div>
-                `}}export{g as default};
--- assets/subscription.index-DueEisme.js
+++
@@ -1,4 +0,0 @@
-import{j as s}from"./mftsccs-browser-kNLGvA95.js";import{m as n}from"./mainView.class-C4NLGCWW.js";import{q as o,H as e}from"./index-CggXStXB.js";import{N as a}from"./navbar.component-BrB-9qRs.js";import"./codemirror-D1H9tReK.js";import"./vendor-DEYWGvhS.js";import"./ckeditor-Cw9bcqxY.js";import"./eslint4b-BLWGvIOa.js";import"./prettier-ClL2wQep.js";import"./loginModal.service-G0xsO5qS.js";class N extends n{constructor(t){super(t),this.setTitle("Subscription")}async getHtml(){const t=await o(),i=await new a(t).getHtml();return setTimeout(async()=>{const r=document.getElementById("subscription-render");await s(e.subscriptionId,r)},0),`
-    ${i}
-        <div id="subscription-render"></div>
-        `}}export{N as default};
