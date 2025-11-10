eae8405eea6b58fa7a1c588ec8969d3c72cb05e0
Deepika-mentorfriends <deepikashrestha@mentorfriends.com>
2025-11-10 14:03:26 +0545

    update

---
+++ assets/subscription.index-BPBuw-91.js
@@ -0,0 +1,4 @@
+import{j as s}from"./mftsccs-browser-kNLGvA95.js";import{m as n}from"./mainView.class-C4NLGCWW.js";import{q as o,H as e}from"./index-DNzZNNPN.js";import{N as a}from"./navbar.component-BBkeaa_F.js";import"./codemirror-D1H9tReK.js";import"./vendor-DEYWGvhS.js";import"./ckeditor-Cw9bcqxY.js";import"./eslint4b-BLWGvIOa.js";import"./prettier-ClL2wQep.js";import"./loginModal.service-BlpFRneN.js";class N extends n{constructor(t){super(t),this.setTitle("Subscription")}async getHtml(){const t=await o(),i=await new a(t).getHtml();return setTimeout(async()=>{const r=document.getElementById("subscription-render");await s(e.subscriptionId,r)},0),`
+    ${i}
+        <div id="subscription-render"></div>
+        `}}export{N as default};
---
+++ assets/video-tutorial.index-DzO8wQm4.js
@@ -0,0 +1,6 @@
+import{m as a}from"./mainView.class-C4NLGCWW.js";import{q as e,H as r}from"./index-DNzZNNPN.js";import{N as m}from"./navbar.component-BBkeaa_F.js";import{j as s}from"./mftsccs-browser-kNLGvA95.js";import"./codemirror-D1H9tReK.js";import"./vendor-DEYWGvhS.js";import"./ckeditor-Cw9bcqxY.js";import"./eslint4b-BLWGvIOa.js";import"./prettier-ClL2wQep.js";import"./loginModal.service-BlpFRneN.js";class H extends a{constructor(t){super(t),this.setTitle("VideoTutorial")}async getHtml(){const t=await e(),o=await new m(t).getHtml();return setTimeout(async()=>{const i=document.getElementById("videoTutorial");i&&await s(r.video_tutorial_widget,i)},0),`
+           ${o}
+            <div class="video-tutorial" id="videoTutorial">
+              <!-- Video tutorial list -->
+            </div>
+        `}}export{H as default};
