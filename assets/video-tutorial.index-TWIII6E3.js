129623448b315c9cadf22890d97fd1e51e20a5cf
Deepika-mentorfriends <deepikashrestha@mentorfriends.com>
2025-12-01 15:51:18 +0545

    aaded wico ai agents. updated codes with claude code

---
+++ assets/video-tutorial.index-TWIII6E3.js
@@ -0,0 +1,6 @@
+import{m as a}from"./mainView.class-C4NLGCWW.js";import{u as e,I as r}from"./index-D8E2bclU.js";import{N as m}from"./navbar.component-UBvhaAV5.js";import{m as s}from"./mftsccs-browser-CcapBeGk.js";import"./codemirror-D1H9tReK.js";import"./vendor-DEYWGvhS.js";import"./ckeditor-Cw9bcqxY.js";import"./eslint4b-BLWGvIOa.js";import"./prettier-ClL2wQep.js";import"./loginModal.service-5dwko9uS.js";class N extends a{constructor(t){super(t),this.setTitle("VideoTutorial")}async getHtml(){const t=await e(),o=await new m(t).getHtml();return setTimeout(async()=>{const i=document.getElementById("videoTutorial");i&&await s(r.video_tutorial_widget,i)},0),`
+           ${o}
+            <div class="video-tutorial" id="videoTutorial">
+              <!-- Video tutorial list -->
+            </div>
+        `}}export{N as default};
