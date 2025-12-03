ccb34210629a36c419739eaf5b582d124791c9c6
Deepika-mentorfriends <deepikashrestha@mentorfriends.com>
2025-12-03 12:35:33 +0545

    updated pwa color

--- assets/loginModal.service-BMh74nTH.js
+++ assets/loginModal.service-BMh74nTH.js
@@ -1,4 +1,4 @@
-import{y as I,C as M}from"./mftsccs-browser-CcapBeGk.js";import{D as g,M as y,I as b,O as w,A as h}from"./index-D8E2bclU.js";class S{constructor(){this.init()}init(){const s=this.getHtml();document.querySelector("body #app")?.insertAdjacentHTML("afterend",s)}getHtml(){return`
+import{y as I,C as M}from"./mftsccs-browser-CcapBeGk.js";import{D as g,M as y,I as b,O as w,A as h}from"./index-tEr-ABoD.js";class S{constructor(){this.init()}init(){const s=this.getHtml();document.querySelector("body #app")?.insertAdjacentHTML("afterend",s)}getHtml(){return`
     <dialog id="login-dialog" class="login-modal">
       <div class="login-container">
         <button class="close-button" type="button">
