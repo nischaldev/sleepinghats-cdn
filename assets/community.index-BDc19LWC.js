00b526e834d6c74b17786a10d6529b86db8775c9
Deepika-mentorfriends <deepikashrestha@mentorfriends.com>
2025-12-22 11:40:49 +0545

    Added cache for the css and html when saving for page

---
+++ assets/community.index-BDc19LWC.js
@@ -0,0 +1,6 @@
+import{m as o}from"./mftsccs-browser-nXWLCvq0.js";import{m as i}from"./mainView.class-C4NLGCWW.js";import{u as a,I as e}from"./index-DvWgoyfM.js";import{N as s}from"./navbar.component-Df7SX9Gf.js";import"./codemirror-D1H9tReK.js";import"./vendor-DEYWGvhS.js";import"./ckeditor-Cw9bcqxY.js";import"./eslint4b-BLWGvIOa.js";import"./prettier-ClL2wQep.js";import"./loginModal.service-C1fbHyIC.js";class g extends i{constructor(t){super(t),this.setTitle("Community")}async getHtml(){const t=await a(),m=await new s(t).getHtml();return setTimeout(async()=>{const n=document.getElementById("communityContent");await o(e.discussion_community,n,"",!1)},0),`
+                    ${m}
+                    <div class="container py-5">
+                        <div id="communityContent"></div>
+                    </div>
+                `}}export{g as default};
