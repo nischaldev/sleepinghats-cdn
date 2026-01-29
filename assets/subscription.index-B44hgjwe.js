ea9633c1ad6d14e09b9ac9750b82b67e4e924648
Deepika-mentorfriends <deepikashrestha@mentorfriends.com>
2026-01-29 11:55:34 +0545

    courses and course pages.

--- assets/subscription.index-B44hgjwe.js
+++
@@ -1,4 +0,0 @@
-import{j as s}from"./mftsccs-browser-D_uubYVV.js";import{m as n}from"./mainView.class-C4NLGCWW.js";import{u as o,I as e}from"./index-CfahUM3g.js";import{N as a}from"./navbar.component-COzeo-44.js";import"./codemirror-D1H9tReK.js";import"./vendor-DEYWGvhS.js";import"./ckeditor-Cw9bcqxY.js";import"./eslint4b-BLWGvIOa.js";import"./prettier-ClL2wQep.js";import"./loginModal.service-Bgs7p_wi.js";class x extends n{constructor(t){super(t),this.setTitle("Subscription")}async getHtml(){const t=await o(),i=await new a(t).getHtml();return setTimeout(async()=>{const r=document.getElementById("subscription-render");await s(e.subscriptionId,r)},0),`
-    ${i}
-        <div id="subscription-render"></div>
-        `}}export{x as default};
