0ba39cbd141cfbeed94f20c15ee3a6ac7e9e5dfc
Deepika-mentorfriends <deepikashrestha@mentorfriends.com>
2026-08-25 17:18:21 +0545

    updates

--- assets/course.index-DBmHCzdr.js
+++
@@ -1,6 +0,0 @@
-import{dT as r}from"./mftsccs-browser-H83tvbKv.js";import{m as i}from"./mainView.class-C4NLGCWW.js";import{y as s,f as a}from"./index-DzL6aS5b.js";import{N as n}from"./navbar.component-DlDmHpmL.js";import"./codemirror-B6qdlLvx.js";import"./vendor-vUhUC0ZM.js";import"./ckeditor-Cw9bcqxY.js";import"./eslint4b-BLWGvIOa.js";import"./prettier-ClL2wQep.js";import"./loginModal.service-b6roBAp9.js";class y extends i{course_id;constructor(t){super(t),this.setTitle("Courses"),this.course_id=Number(t.id)}async getHtml(){const t=await s(),e=await new n(t).getHtml();return setTimeout(async()=>{const o=document.getElementById("wico_course_container");await r(a.course_widget,o,{courseId:this.course_id},!1)},0),`
-      ${e}
-      <div class="container py-5">
-          <div id="wico_course_container"></div>
-      </div>
-    `}}export{y as default};
--- assets/video-tutorial.index-Bqub2h3a.js
+++
@@ -1,6 +0,0 @@
-import{m as a}from"./mainView.class-C4NLGCWW.js";import{y as e,f as r}from"./index-DzL6aS5b.js";import{N as m}from"./navbar.component-DlDmHpmL.js";import{dT as s}from"./mftsccs-browser-H83tvbKv.js";import"./codemirror-B6qdlLvx.js";import"./vendor-vUhUC0ZM.js";import"./ckeditor-Cw9bcqxY.js";import"./eslint4b-BLWGvIOa.js";import"./prettier-ClL2wQep.js";import"./loginModal.service-b6roBAp9.js";class y extends a{constructor(t){super(t),this.setTitle("VideoTutorial")}async getHtml(){const t=await e(),o=await new m(t).getHtml();return setTimeout(async()=>{const i=document.getElementById("videoTutorial");i&&await s(r.video_tutorial_widget,i,"",!1)},0),`
-           ${o}
-            <div class="video-tutorial" id="videoTutorial">
-              <!-- Video tutorial list -->
-            </div>
-        `}}export{y as default};
